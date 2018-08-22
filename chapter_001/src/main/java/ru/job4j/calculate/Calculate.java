5f5ab061688be9b80e516bf87359440756e5de77
Denis Popkov <pkvdenis@gmail.com>
2018-08-22 19:42:55 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ * @author Denis Popkov
+ */
 public class Calculate {
     
+    /**
+     * Entry point to application.
+     * @param args array of string arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
