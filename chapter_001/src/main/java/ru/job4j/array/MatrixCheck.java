0c957e5acf8aa7682341da165233a16889733163
Denis Popkov <pkvdenis@gmail.com>
2018-08-31 13:49:34 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям. [#53859]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+/**
+ * Contains method for checking diagonals of matrix.
+ *
+ * @author Denis Popkov
+ */
+public class MatrixCheck {
+    /**
+     * Checks whether every diagonal is mono that is it contains only same elements
+     * (all true, all false, or left diagonal true and right false, or left false and right true).
+     * @param data 2d array of elements
+     * @return true if all diagonals are mono or false otherwise
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        int size = data.length, i, j;
+        boolean first = data[0][0];
+        for (i = 1, j = 1; i < size; i++, j++) {
+            if (data[i][j] != first) {
+                result = false;
+                break;
+            }
+        }
+        first = data[0][size - 1];
+        for (i = 1, j = size - 2; i < size; i++, j--) {
+            if (data[i][j] != first) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Denis Popkov
+ */
+public class MatrixCheckTest {
+    @Test
+    public void whenAllDiagonalsTrueThenTrue() {
+        MatrixCheck matrixCheck = new MatrixCheck();
+        boolean[][] input = {
+                {true, false, true},
+                {false, true, false},
+                {true, false, true}
+        };
+        boolean result = matrixCheck.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotAllDiagonalsTrueThenFalse() {
+        MatrixCheck matrixCheck = new MatrixCheck();
+        boolean[][] input = {
+                {true, false, true},
+                {false, true, false},
+                {true, false, false}
+        };
+        boolean result = matrixCheck.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenAllDiagonalsFalseThenTrue() {
+        MatrixCheck matrixCheck = new MatrixCheck();
+        boolean[][] input = {
+                {false, true, false},
+                {true, false, true},
+                {false, true, false}
+        };
+        boolean result = matrixCheck.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotAllDiagonalsFalseThenFalse() {
+        MatrixCheck matrixCheck = new MatrixCheck();
+        boolean[][] input = {
+                {true, true, false},
+                {true, false, true},
+                {false, true, false}
+        };
+        boolean result = matrixCheck.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDifferentDiagonalsThenTrue() {
+        MatrixCheck matrixCheck = new MatrixCheck();
+        boolean[][] input = {
+                {true, false},
+                {false, true}
+        };
+        boolean result = matrixCheck.mono(input);
+        assertThat(result, is(true));
+    }
+}
