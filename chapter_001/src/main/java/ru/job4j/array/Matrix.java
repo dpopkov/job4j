5938e841fd0ffd96bee083e2173092637f56fc33
Denis Popkov <pkvdenis@gmail.com>
2018-08-31 13:18:34 +0300

    6.6. Двухмерный массив. Таблица умножения. [#33491]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+/**
+ * Contains method for creating multiplication table.
+ *
+ * @author Denis Popkov
+ */
+public class Matrix {
+    /**
+     * Creates a multiplication table of specified size.
+     * @param size size of the table
+     * @return multiplication table
+     */
+    public int[][] multiply(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Denis Popkov
+ */
+public class MatrixTest {
+    @Test
+    public void whenSize1ThenTable1() {
+        Matrix matrix = new Matrix();
+        int[][] result = matrix.multiply(1);
+        int[][] expected = {
+                {1},
+        };
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSize5ThenTable1to25() {
+        Matrix matrix = new Matrix();
+        int[][] result = matrix.multiply(5);
+        int[][] expected = {
+                {1, 2, 3, 4, 5},
+                {2, 4, 6, 8, 10},
+                {3, 6, 9, 12, 15},
+                {4, 8, 12, 16, 20},
+                {5, 10, 15, 20, 25}
+        };
+        assertThat(result, is(expected));
+    }
+}
