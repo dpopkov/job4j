f54f7a9fee748dcf10212befce95cd45307045ca
Denis Popkov <pkvdenis@gmail.com>
2018-09-12 17:33:56 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,39 +1,47 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 @SuppressWarnings("StringBufferReplaceableByString")
 public class PaintTest {
+    private final PrintStream stdOut = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void setStandardOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void restoreStandardOutput() {
+        System.setOut(this.stdOut);
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdOut = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square(2));
-        String expected = new StringBuilder()
-                .append("**").append(Shape.NEW_LINE)
-                .append("**").append(Shape.NEW_LINE).toString();
-        assertThat(out.toString(), is(expected));
-        System.setOut(stdOut);
+        String expected = new StringJoiner(Shape.NEW_LINE, "", Shape.NEW_LINE)
+                .add("**")
+                .add("**").toString();
+        assertThat(this.out.toString(), is(expected));
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdOut = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle(3));
-        String expected = new StringBuilder()
-                .append("  *  ").append(Shape.NEW_LINE)
-                .append(" *** ").append(Shape.NEW_LINE)
-                .append("*****").append(Shape.NEW_LINE).toString();
-        assertThat(out.toString(), is(expected));
-        System.setOut(stdOut);
+        String expected = new StringJoiner(Shape.NEW_LINE, "", Shape.NEW_LINE)
+                .add("  *  ")
+                .add(" *** ")
+                .add("*****").toString();
+        assertThat(this.out.toString(), is(expected));
     }
 }
