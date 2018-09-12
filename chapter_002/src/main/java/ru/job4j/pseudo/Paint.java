79a17fa6771fc831cfa68bf8b9db3782b2ec5ac7
Denis Popkov <pkvdenis@gmail.com>
2018-09-12 17:14:01 +0300

    Корректировка: добавить JavaDoc в классе Paint.

--- chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -1,6 +1,13 @@
 package ru.job4j.pseudo;
 
+/**
+ * Contains method for printing pseudo-graphic shapes.
+ */
 public class Paint {
+    /**
+     * Prints shape in standard console.
+     * @param shape shape
+     */
     public void draw(Shape shape) {
         System.out.print(shape.draw());
     }
