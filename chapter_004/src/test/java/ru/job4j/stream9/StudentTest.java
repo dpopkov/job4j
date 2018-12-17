e7ccbb7cb13eef79489bbc942eaa1794c3eb9972
Denis Popkov <pkvdenis@gmail.com>
2018-12-17 16:44:45 +0300

    Add tests to StudentTest in improve coverage.

--- chapter_004/src/test/java/ru/job4j/stream9/StudentTest.java
+++ chapter_004/src/test/java/ru/job4j/stream9/StudentTest.java
@@ -16,4 +16,37 @@ public class StudentTest {
         assertThat(bob1.compareTo(new Student("Bob", 1)), is(0));
         assertTrue(new Student("Alice", 1).compareTo(bob1) < 0);
     }
+
+    @Test
+    public void testEquals() {
+        Student bob1 = new Student("Bob", 42);
+        Student bob2 = new Student("Bob", 42);
+        assertEquals(bob1, bob2);
+    }
+
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    @Test
+    public void whenSameInstanceThenEqualsReturnsTrue() {
+        Student bob1 = new Student("Bob", 42);
+        Student bob2 = bob1;
+        assertEquals(bob1, bob2);
+    }
+
+    @Test
+    public void whenOtherTypeThenEqualsReturnsFalse() {
+        assertNotEquals("Bob", new Student("Bob", 42));
+    }
+
+    @Test
+    public void whenEqualsThenSameHashCodes() {
+        Student bob1 = new Student("Bob", 42);
+        Student bob2 = new Student("Bob", 42);
+        assertEquals(bob1.hashCode(), bob2.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        Student bob = new Student("Bob", 42);
+        assertThat(bob.toString(), is("Student{name='Bob', score=42}"));
+    }
 }
