ef702284caf8c5ff47746c7c499496e7e28029b0
Denis Popkov <pkvdenis@gmail.com>
2018-12-18 21:58:56 +0300

    2. Local-Variable Type Inference [#70626]

--- chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -2,9 +2,6 @@ package ru.job4j.search;
 
 import org.junit.Test;
 
-import java.util.Iterator;
-import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,28 +9,26 @@ public class PhoneDictionaryTest {
 
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("John", "Doe", "1234567", "Liverpool"));
-        List<Person> found = phones.find("John");
-        assertThat(found.iterator().next().getSurname(), is("Doe"));
+        assertThat(phones.find("John").iterator().next().getSurname(), is("Doe"));
     }
 
     @Test
     public void whenFindBySurname() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("John", "Doe", "1234567", "Liverpool"));
         phones.add(new Person("Jack", "Dawson", "1234567", "Liverpool"));
-        List<Person> found = phones.find("Daw");
-        assertThat(found.iterator().next().getName(), is("Jack"));
+        assertThat(phones.find("Daw").iterator().next().getName(), is("Jack"));
     }
 
     @Test
     public void whenFindByAddress() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("John", "Doe", "1234567", "Moscow city"));
         phones.add(new Person("Jack", "Dawson", "1234568", "Moscow"));
         phones.add(new Person("Ivan", "Ivanov", "1234569", "Moscow city"));
-        Iterator<Person> iterator = phones.find("Moscow").iterator();
+        var iterator = phones.find("Moscow").iterator();
         assertThat(iterator.next().getName(), is("John"));
         assertThat(iterator.next().getName(), is("Jack"));
         assertThat(iterator.next().getPhone(), is("1234569"));
@@ -41,21 +36,20 @@ public class PhoneDictionaryTest {
 
     @Test
     public void whenFindByPhone() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("John", "Doe", "1234567", "Moscow city"));
         phones.add(new Person("Jack", "Dawson", "234568", "Moscow city"));
         phones.add(new Person("Ivan", "Ivanov", "1234569", "Paris"));
-        Iterator<Person> iterator = phones.find("1234").iterator();
+        var iterator = phones.find("1234").iterator();
         assertThat(iterator.next().getName(), is("John"));
         assertThat(iterator.next().getAddress(), is("Paris"));
     }
 
     @Test
     public void whenFindNonExistingKeyThenReturnEmptyList() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("John", "Doe", "1234567", "Moscow city"));
         phones.add(new Person("Jack", "Dawson", "234568", "Moscow city"));
-        List<Person> found = phones.find("9876");
-        assertThat(found.size(), is(0));
+        assertThat(phones.find("9876").size(), is(0));
     }
 }
--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -8,31 +8,28 @@ import static org.junit.Assert.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenOneTask() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("one", 5));
-        Task result = queue.take();
-        assertThat(result.getDesc(), is("one"));
+        assertThat(queue.take().getDesc(), is("one"));
     }
 
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
-        assertThat(result.getDesc(), is("urgent"));
+        assertThat(queue.take().getDesc(), is("urgent"));
     }
 
     @Test
     public void whenLowerPriorityInsertedLast() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("middle", 3));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("low", 5));
         queue.take();
         queue.take();
-        Task result = queue.take();
-        assertThat(result.getDesc(), is("low"));
+        assertThat(queue.take().getDesc(), is("low"));
     }
 }
