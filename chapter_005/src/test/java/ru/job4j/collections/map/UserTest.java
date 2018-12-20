2a69d6e5ffc0445d31629c437b61dcc6ca0b04b8
Denis Popkov <pkvdenis@gmail.com>
2018-12-20 22:06:12 +0300

    2. Не перекрывать equals hashCode[#86545]

--- chapter_005/src/test/java/ru/job4j/collections/map/UserTest.java
+++ chapter_005/src/test/java/ru/job4j/collections/map/UserTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
@@ -23,5 +25,20 @@ public class UserTest {
         Calendar birthday = new GregorianCalendar(1998, Calendar.DECEMBER, 20);
         User alice = new User("Alice", 1, birthday);
         assertThat(alice.toString(), is("User{name='Alice', children=1, birthday=20.12.1998}"));
+    }
+
+    @Test
+    public void whenEqualsAndHashcodeAreNotOverriddenThenMapContainsTwoObjects() {
+        Calendar birthday1 = new GregorianCalendar(1998, Calendar.DECEMBER, 20);
+        Calendar birthday2 = new GregorianCalendar(1998, Calendar.DECEMBER, 20);
+        User alice1 = new User("Alice", 1, birthday1);
+        User alice2 = new User("Alice", 1, birthday2);
+        Map<User, String> map = new HashMap<>();
+        map.put(alice1, "First");
+        map.put(alice2, "Second");
+        assertThat(map.size(), is(2));
+        assertThat(map.get(alice1), is("First"));
+        assertThat(map.get(alice2), is("Second"));
+        System.out.println(map.toString());
     }
 }
