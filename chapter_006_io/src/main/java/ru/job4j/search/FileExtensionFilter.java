e2f0257f205657e948e026ce63367340a065493a
Denis Popkov <pkvdenis@gmail.com>
2019-02-20 02:09:25 +0300

    добавить методы в FileExtensionFilter для зад. 4. Архивировать проект.  [#861]

--- chapter_006_io/src/main/java/ru/job4j/search/FileExtensionFilter.java
+++ chapter_006_io/src/main/java/ru/job4j/search/FileExtensionFilter.java
@@ -2,6 +2,7 @@ package ru.job4j.search;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,26 +12,50 @@ import java.util.Set;
  */
 public class FileExtensionFilter implements FileFilter {
     /** Set of extensions. */
-    private final Set<String> extSet;
+    private Set<String> extSet;
 
     /**
      * Constructs the filter using the specified list of extensions.
      * @param extensions list of extensions
      */
     public FileExtensionFilter(List<String> extensions) {
-        this.extSet = new HashSet<>(extensions);
+        if (extensions != null) {
+            this.extSet = new HashSet<>(extensions);
+        }
     }
 
     /**
      * Tests whether or not the specified abstract pathname represents a file
      * with extension equal to an extension from the list of this filter.
-     * @param pathname The abstract pathname to be tested
+     * @param file The abstract pathname to be tested
      * @return <code>true</code> if and only if <code>pathname</code>
      * should be accepted
      */
     @Override
-    public boolean accept(File pathname) {
-        return extSet.contains(getExtension(pathname.getName()));
+    public boolean accept(File file) {
+        return extSet == null || extSet.contains(getExtension(file.getName()));
+    }
+
+    /**
+     * Tests whether or not the specified path represents a file
+     * with extension equal to an extension from the list of this filter.
+     * @param path The path to be tested
+     * @return <code>true</code> if and only if <code>path</code>
+     * should be accepted
+     */
+    public boolean accept(Path path) {
+        return extSet == null || extSet.contains(getExtension(path.getFileName().toString()));
+    }
+
+    /**
+     * Tests whether or not the specified path represents a file
+     * with extension not equal to any extension from the list of this filter.
+     * @param path The path to be tested
+     * @return <code>true</code> if and only if <code>path</code>
+     * should be rejected
+     */
+    public boolean reject(Path path) {
+        return !accept(path);
     }
 
     /**
--- chapter_006_io/src/test/java/ru/job4j/search/FileExtensionFilterTest.java
+++ chapter_006_io/src/test/java/ru/job4j/search/FileExtensionFilterTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.hamcrest.core.Is.*;
@@ -21,5 +22,51 @@ public class FileExtensionFilterTest {
     public void whenAcceptFileWithNonExistingExtensionThenFalse() {
         FileFilter filter = new FileExtensionFilter(List.of("class, xls"));
         assertThat(filter.accept(new File("1.doc")), is(false));
+    }
+
+    @Test
+    public void whenFilterWithNullParameterThenAcceptsAllFiles() {
+        FileFilter filter = new FileExtensionFilter(null);
+        assertThat(filter.accept(new File("1.doc")), is(true));
+        assertThat(filter.accept(new File("2.txt")), is(true));
+        assertThat(filter.accept(new File("3.tmp")), is(true));
+    }
+
+    @Test
+    public void whenAcceptPathWithExistingExtensionThenTrue() {
+        FileExtensionFilter filter = new FileExtensionFilter(List.of("txt"));
+        assertThat(filter.accept(Paths.get("dir/1.txt")), is(true));
+    }
+
+    @Test
+    public void whenAcceptPathWithNonExistingExtensionThenFalse() {
+        FileExtensionFilter filter = new FileExtensionFilter(List.of("class, xls"));
+        assertThat(filter.accept(Paths.get("1.doc")), is(false));
+    }
+
+    @Test
+    public void whenFilterWithNullParameterThenAcceptsAllPaths() {
+        FileExtensionFilter filter = new FileExtensionFilter(null);
+        assertThat(filter.accept(Paths.get("1.doc")), is(true));
+        assertThat(filter.accept(Paths.get("2.txt")), is(true));
+    }
+
+    @Test
+    public void whenRejectPathWithExistingExtensionThenFalse() {
+        FileExtensionFilter filter = new FileExtensionFilter(List.of("txt"));
+        assertThat(filter.reject(Paths.get("dir/1.txt")), is(false));
+    }
+
+    @Test
+    public void whenRejectPathWithNonExistingExtensionThenTrue() {
+        FileExtensionFilter filter = new FileExtensionFilter(List.of("txt"));
+        assertThat(filter.reject(Paths.get("dir/1.doc")), is(true));
+    }
+
+    @Test
+    public void whenFilterWithNullParameterThenRejectAllPaths() {
+        FileExtensionFilter filter = new FileExtensionFilter(null);
+        assertThat(filter.reject(Paths.get("1.doc")), is(false));
+        assertThat(filter.reject(Paths.get("2.txt")), is(false));
     }
 }
